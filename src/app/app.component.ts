6b8f51c2f99606732859af2bc6777f6664fb5535
Pieter-Willem Bekaert <pieterwillem.bekaert@gmail.com>
2020-11-19 23:37:04 +0100

    angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OnzeTokDeWereldRond';
+
+  mobileQuery: MediaQueryList;
+
+fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+
+private _mobileQueryListener: () => void;
+
+constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+  this.mobileQuery.addListener(this._mobileQueryListener);
+
+}
+
+ngOnDestroy():void{
+  this.mobileQuery.removeListener(this._mobileQueryListener);
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,112 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { MatSliderModule } from '@angular/material/slider';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { LayoutModule } from '@angular/cdk/layout';
+import { HomeComponent } from './home/home.component';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { KaartComponent } from './kaart/kaart.component';
+import { PrikbordComponent } from './prikbord/prikbord.component';
+import { OverOnsComponent } from './over-ons/over-ons.component';
+import { TekenOnzeTokComponent } from './teken-onze-tok/teken-onze-tok.component';
+import { VolgOnsComponent } from './volg-ons/volg-ons.component';
+
+import { MatCarouselModule } from '@ngmodule/material-carousel';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { WavesModule, ButtonsModule, IconsModule } from 'angular-bootstrap-md'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    KaartComponent,
+    PrikbordComponent,
+    OverOnsComponent,
+    TekenOnzeTokComponent,
+    VolgOnsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    WavesModule,
+    ButtonsModule,
+    IconsModule,
+    BrowserModule,
+    RouterModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatSelectModule, 
+    MatInputModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,  
+    FormsModule,
+    MatSliderModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    FlexLayoutModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'kaart', component: KaartComponent },
+      { path: 'OverOns', component: OverOnsComponent },
+      { path: 'Prikbord', component: PrikbordComponent },
+      { path: 'TekenOnzeTok', component: TekenOnzeTokComponent },
+
+
+    ]),
+    BrowserAnimationsModule,
+    MatGridListModule,
+    LayoutModule,
+    DragDropModule,
+    MatTreeModule,
+    MatListModule,
+    MatCarouselModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
